a05bf64b404cc539373a924991c0f28f0ae6d469
Antanas <servisas911@gmail.com>
2024-01-21 16:42:06 +0200

    1d

--- views/test.php
+++ views/test.php
@@ -2,9 +2,7 @@
            
 <div style="width: 40%; margin: 2em 3em; background-color: rgba(255, 255, 255, 0.3); padding: 20px; border-radius: 10px;">
 
-        <h4>The mission of the Bank of Future is to bring benefits to society. To accomplish this, we rely on highly-qualified employees,
-            financial and technological resources. As part of the Eurosystem, we cooperate with the European Central Bank and other euro area
-            central banks to make decisions on euro area monetary policy and its implementation.
+        <h4>Not Bank yra finansinių technologijų įmonė, kuri siūlo vartotojams finansines paslaugas, kurios yra paprastesnės, skaidresnės ir pigesnės nei tradicinių bankų. Įneši - neišsiimsi!
         </h4>
      
    </div>      
