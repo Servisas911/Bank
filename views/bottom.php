82bef5404194ed4bab73524b134e4450bb162a08
Antanas <servisas911@gmail.com>
2024-01-21 17:03:21 +0200

    final

--- views/bottom.php
+++ views/bottom.php
@@ -1,6 +1,6 @@
 <footer style="text-align: center;  margin-top: auto; color:white; margin-bottom:0">
     <hr>
-    <span>Not Bank end 2024 </span>
+    <span>Not a Bank - 2024 end.. </span>
 </footer>
 </body>
 
--- views/test.php
+++ views/test.php
@@ -2,7 +2,7 @@
            
 <div style="width: 40%; margin: 2em 3em; background-color: rgba(255, 255, 255, 0.3); padding: 20px; border-radius: 10px;">
 
-        <h4>Not Bank yra finansinių technologijų įmonė, kuri siūlo vartotojams finansines paslaugas, kurios yra paprastesnės, skaidresnės ir pigesnės nei tradicinių bankų. Įneši - neišsiimsi!
+        <h4>Not a Bank yra finansinių technologijų įmonė, kuri siūlo vartotojams finansines paslaugas, kurios yra paprastesnės, skaidresnės ir pigesnės nei tradicinių bankų. Įneši - neišsiimsi!
         </h4>
      
    </div>      
